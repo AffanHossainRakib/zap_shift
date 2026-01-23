6ab7d0dc4774b9829b6ff849e8fde3a249fa84f1
Md. Affan Hossain Rakib <affanhossainrakib@gmail.com>
2026-01-24 00:04:54 +0600

    feat: create Navbar component and integrate Logo with responsive design

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,17 @@
 import { Link } from "react-router";
 import logo from "../../assets/logo.png";
+
 const Logo = () => {
   return (
     <Link to="/" className="flex items-end">
-      <img src={logo} alt="Zap Shift Logo" />
-      <h3 className="text-3xl font-bold -ms-2.5">ZapShift</h3>
+      <img
+        src={logo}
+        alt="Zap Shift Logo"
+        className="w-8 h-8 sm:w-10 sm:h-10 object-contain"
+      />
+      <h3 className="text-xl sm:text-3xl font-bold -ms-2.5 whitespace-nowrap">
+        ZapShift
+      </h3>
     </Link>
   );
 };
---
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,65 @@
+import Logo from "../../../components/Logo/Logo";
+import { NavLink } from "react-router";
+import PrimaryBtn from "../../../components/PrimaryBtn/PrimaryBtn";
+import SecondaryBtn from "../../../components/SecondaryBtn/SecondaryBtn";
+import { BsArrowUpRightCircleFill } from "react-icons/bs";
+import { VscThreeBars } from "react-icons/vsc";
+
+const Navbar = () => {
+  const links = (
+    <>
+      <li>
+        <NavLink to="/services">Services</NavLink>
+      </li>
+      <li>
+        <NavLink to="/coverage">Coverage</NavLink>
+      </li>
+      <li>
+        <NavLink to="/about">About Us</NavLink>
+      </li>
+      <li>
+        <NavLink to="/pricing">Pricing</NavLink>
+      </li>
+      <li>
+        <NavLink to="/blog">Blog</NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="sticky top-4 z-50 ">
+      <div className="navbar bg-white shadow-sm rounded-lg">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <VscThreeBars className="h-6 w-6 fill-primary-content" />
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow text-secondary-content"
+            >
+              {links}
+            </ul>
+          </div>
+
+          <Logo className="w-full h-full object-contain" />
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 text-secondary-content">
+            {links}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <div className="mr-2">
+            <SecondaryBtn to="/login" message="Sign In" />
+          </div>
+          <div className="flex items-center">
+            <PrimaryBtn to="/register" message="Sign Up" />
+            <BsArrowUpRightCircleFill className="h-10 w-10 bg-primary rounded-full hidden md:block" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
