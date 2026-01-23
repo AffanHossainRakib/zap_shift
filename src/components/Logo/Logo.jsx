5367f6f61da49159c4b6ca8df9a52468c66c12cf
Md. Affan Hossain Rakib <affanhossainrakib@gmail.com>
2026-01-23 23:49:56 +0600

    feat: update Logo component to use Link for navigation

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router";
 import logo from "../../assets/logo.png";
 const Logo = () => {
   return (
-    <div className="flex items-end">
+    <Link to="/" className="flex items-end">
       <img src={logo} alt="Zap Shift Logo" />
       <h3 className="text-3xl font-bold -ms-2.5">ZapShift</h3>
-    </div>
+    </Link>
   );
 };
 
