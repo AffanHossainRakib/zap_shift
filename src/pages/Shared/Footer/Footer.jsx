0bdcb99a5223b6c55c35bc54aba70540cefde7fc
Md. Affan Hossain Rakib <affanhossainrakib@gmail.com>
2026-01-23 22:49:00 +0600

    feat: add Logo and Footer components, update routing and styles

---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,29 @@
+import Logo from "../../../components/Logo/Logo";
+import { FaFacebook, FaX, FaYoutube } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+
+const Footer = () => {
+  return (
+    <footer className="footer footer-horizontal bg-[#0B0B0B] text-primary-content footer-center p-10 rounded-3xl">
+      <aside className="max-w-3xl mx-auto">
+        <Logo />
+
+        <p className="text-xs md:text-sm">
+          Enjoy fast, reliable parcel delivery with real-time tracking and zero
+          hassle. From personal packages to business shipments — we deliver on
+          time, every time.
+        </p>
+      </aside>
+      <nav>
+        <div className="grid grid-flow-col gap-4">
+          <FaFacebook className="w-6 h-6" />
+          <FaYoutube className="w-6 h-6" />
+          <FaX className="w-6 h-6" />
+          <MdEmail className="w-6 h-6" />
+        </div>
+      </nav>
+    </footer>
+  );
+};
+
+export default Footer;
