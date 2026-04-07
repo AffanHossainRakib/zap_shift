7bf3b93d637e7e3554cc3ad486abf665d7755c04
AffanHossainRakib <affanhossainrakib@gmail.com>
2026-04-07 16:31:21 +0600

    feat: implement parcel deletion with confirmation dialog in MyParcels component

--- src/pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/pages/Dashboard/MyParcels/MyParcels.jsx
@@ -2,18 +2,59 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FiEdit } from "react-icons/fi";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { FaRegTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const MyParcels = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
 
-  const { data: myParcels = [] } = useQuery({
+  const { data: myParcels = [], refetch } = useQuery({
     queryKey: ["my-parcels", user.email],
     queryFn: async () => {
       const res = await axiosSecure.get(`/parcels?email=${user.email}`);
       return res.data;
     },
   });
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure you want to delete this parcel?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Delete Parcel",
+      cancelButtonText: "I will keep it",
+      reverseButtons: true,
+      buttonsStyling: false,
+      customClass: {
+        title: "font-extrabold text-secondary",
+        confirmButton:
+          "rounded-xl border-0 bg-red-500/80 px-4 py-2.5 font-bold text-white transition hover:bg-red-600 ml-2",
+        cancelButton:
+          "rounded-xl border-0 bg-slate-200 px-4 py-2.5 font-bold text-secondary",
+      },
+    }).then((result) => {
+      if (result.isConfirmed)
+        axiosSecure.delete(`/parcels/${id}`).then((res) => {
+          refetch();
+          Swal.fire({
+            title: "Parcel Deleted",
+            html: `<div class="mt-2 text-slate-700">Your parcel has been deleted successfully. </div>`,
+            icon: "success",
+            confirmButtonText: "Great",
+            buttonsStyling: false,
+            customClass: {
+              popup: "rounded-2xl border border-slate-200 px-5 py-6",
+              title: "font-extrabold text-secondary",
+              confirmButton:
+                "rounded-xl border-0 bg-secondary px-4 py-2.5 font-bold text-white transition hover:brightness-110",
+            },
+          });
+        });
+    });
+  };
 
   return (
     <div className="my-10 mx-5 bg-white p-5 sm:p-10 rounded-3xl shadow-lg">
@@ -40,9 +81,18 @@ const MyParcels = () => {
                 <td>{parcel.parcelName}</td>
                 <td>{parcel.cost}</td>
                 <td>Pending</td>
-                <td>
-                  <button className="btn btn-primary btn-sm">
-                    View Details
+                <td className="flex gap-1">
+                  <button className="btn btn-square hover:bg-primary">
+                    <FiEdit />
+                  </button>
+                  <button className="btn btn-square hover:bg-primary">
+                    <FaMagnifyingGlass />
+                  </button>
+                  <button
+                    onClick={() => handleDelete(parcel._id)}
+                    className="btn btn-square hover:bg-primary"
+                  >
+                    <FaRegTrashAlt />
                   </button>
                 </td>
               </tr>
