17752eb81d91b97f0d4070026e2798b7a2702f40
AffanHossainRakib <affanhossainrakib@gmail.com>
2026-04-07 15:30:18 +0600

    feat: enhance MyParcels component to display user parcels with improved data fetching and layout

--- src/pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/pages/Dashboard/MyParcels/MyParcels.jsx
@@ -7,18 +7,51 @@ const MyParcels = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
 
-  const { data } = useQuery({
-    queryKey: [user.email, "parcels"],
-    queryFn: () => {
-      return axiosSecure
-        .get(`/parcels?email=${user.email}`)
-        .then((res) => res.data);
+  const { data: myParcels = [] } = useQuery({
+    queryKey: ["my-parcels", user.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/parcels?email=${user.email}`);
+      return res.data;
     },
   });
 
-  console.log("My Parcels:", data);
+  return (
+    <div className="my-10 mx-5 bg-white p-5 sm:p-10 rounded-3xl shadow-lg">
+      <h2 className="text-xl sm:text-2xl mb-10 text-secondary font-bold">
+        {user.displayName} has total of {myParcels.length} parcels
+      </h2>
 
-  return <div>MyParcels {data?.length || 0}</div>;
+      <div className="overflow-x-auto">
+        <table className="table table-zebra bg-secondary/10 w-full">
+          {/* head */}
+          <thead className="bg-secondary text-white">
+            <tr>
+              <th>SL. </th>
+              <th>Parcel Name</th>
+              <th>Cost</th>
+              <th>Payment Status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {myParcels.map((parcel, index) => (
+              <tr key={parcel._id}>
+                <th>{index + 1}</th>
+                <td>{parcel.parcelName}</td>
+                <td>{parcel.cost}</td>
+                <td>Pending</td>
+                <td>
+                  <button className="btn btn-primary btn-sm">
+                    View Details
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyParcels;
