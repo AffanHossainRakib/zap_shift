7c0dfb6ca8bb7d953d5365836b21da2fe08c051e
AffanHossainRakib <affanhossainrakib@gmail.com>
2026-04-12 16:46:26 +0600

    feat: enhance PaymentSuccess component to display tracking and payment IDs after successful payment

--- src/pages/Dashboard/Payment/PaymentSuccess.jsx
+++ src/pages/Dashboard/Payment/PaymentSuccess.jsx
@@ -2,9 +2,11 @@ import React, { useEffect } from "react";
 import { useSearchParams } from "react-router";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { toast } from "sonner";
+import { useState } from "react";
 
 const PaymentSuccess = () => {
   const [searchParams] = useSearchParams();
+  const [paymentInfo, setPaymentInfo] = useState(null);
   const sessionId = searchParams.get("session_id");
   const axiosSecure = useAxiosSecure();
 
@@ -13,18 +15,23 @@ const PaymentSuccess = () => {
       axiosSecure
         .patch(`/payment-success?sessionId=${sessionId}`)
         .then((res) => {
-          toast.success(`Payment success response: ${res.data}`);
+          setPaymentInfo(res.data);
         })
         .catch((error) => {
           toast.error("Error verifying payment success:", error);
         });
     }
   }, [axiosSecure, sessionId]);
-  toast.success(`Checkout Session ID: ${sessionId}`);
   return (
-    <div className="h-[calc(100vh-64px)] flex items-center justify-center">
+    <div className="h-[calc(100vh-64px)] flex flex-col items-center justify-center text-center px-10">
       <p className="text-3xl text-secondary-content-content font-bold text-center">
         Your payment was successful! Thank you for using our service.
+      </p>
+      <p className="text-lg text-bold text-secondary-content">
+        Your tracking ID is: {paymentInfo?.trackingId}
+      </p>
+      <p className="text-lg text-bold text-secondary-content">
+        Your payment ID is: {paymentInfo?.paymentId}
       </p>
     </div>
   );
